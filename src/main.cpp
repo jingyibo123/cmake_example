782631cf2b0ad01494babccd6c44935da69746b5
jingyibo123 <jingyibo123@hotmail.com>
2021-02-16 20:23:30 +0800

    t
    ttt

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
+#include "add.h"
 #include <pybind11/pybind11.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
-
-int add(int i, int j) {
-    return i + j;
-}
 
 namespace py = pybind11;
 
